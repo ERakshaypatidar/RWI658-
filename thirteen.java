7cbd53e8be20e03f7ba98e837b2daf8d36295e4f
ERakshaypatidar <142728160+ERakshaypatidar@users.noreply.github.com>
2024-06-11 09:47:37 +0530

    Add files via upload
    ARRAYS STRING AND THIS KIYWORD

---
+++ thirteen.java
@@ -0,0 +1,20 @@
+package RAILW;
+
+public class thirteen {
+		
+void show()
+{
+	System.out.println(this);
+}
+}
+class c extends thirteen
+{
+	public static void main(String[]args)
+	
+	{
+		c str=new c();
+		System.out.println(str);
+		str.show();
+	}
+	
+}
